bf95dfdeab5c8d348078037242b346cc61ebe8f7
tuokaikyle <867367934@qq.com>
2020-12-28 17:04:43 +1000

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import Tomato from './models/tomatoModel.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config({ path: './.env' });
 
@@ -63,10 +64,10 @@ app.put('/edittomato', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 if (process.env.NODE_ENV == 'production') {
   // 要使用static build
   app.use(express.static('client/build'));
-  const path = require('path');
   // 任意route, 都send index.html
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
